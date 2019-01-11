7e273accff0e45ecd0494d0b5cfccf931926f275
Dylan <kite.d92@gmail.com>
2019-01-11 12:51:12 -0800

    added logout feature

--- my-app/src/ProtectedPage.js
+++ my-app/src/ProtectedPage.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {logOut} from './actions/auth'
+import {bindActionCreators} from 'redux'
 
-function ProtectedPage(props){
-    return(
-        <div className="protectedPage">
-            <h1>We're in</h1>
-            <div>Welcome, ${props.auth.user.f_name}. Your token is stored in local storage under the key 'token'</div>    
-        </div>
-    )
+class ProtectedPage extends Component{
+    handleLogOut = () => {
+        this.props.logOut()
+        this.props.history.push('/')
+    }
+
+    render(){
+        return(
+            <div className="protectedPage">
+                <h1>We're in</h1>
+                <div>Welcome, ${this.props.auth.user.f_name}. Your token is stored in local storage under the key 'token'</div>    
+                <p className='logout' onClick={this.handleLogOut}>Log Out</p>
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = state => ({auth:state.auth})
+const mapDispatchToProps = dispatch => bindActionCreators({logOut}, dispatch)
 
-export default connect(mapStateToProps)(ProtectedPage)
+export default connect(mapStateToProps, mapDispatchToProps)(ProtectedPage)

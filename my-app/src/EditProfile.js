99d0bb18d7c638d2346e00730745da152ddf0375
Dylan <kite.d92@gmail.com>
2019-01-11 17:39:11 -0800

    began user editing page

--- my-app/src/EditProfile.js
+++ my-app/src/EditProfile.js
@@ -2,11 +2,30 @@ import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 
 class EditProfile extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+    componentDidMount(){
+
+    }
+
     render(){
         return (
             <div className="editProfile">
-                <Link to='/home'>Back</Link>
+                <Link to='/home'><i className="fa fa-arrow-left"></i></Link>
                 <div>Edit Profile Page</div>
+                <form>
+                    <input type="text" name="f_name" required />
+                    <input type="text" name="l_name" required />
+                    <input type="password" name="oldPassword" required />
+                    <input type="password" name="newPassword" required />
+                    <input type="password" name="passwordMatch" required />
+                    <input type="file" name="img" required/>
+                    
+                </form>
             </div>
         )
     }
